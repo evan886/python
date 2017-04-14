a06b92635e3ae21ab7c15a042d18eca02deb9b04
evan <evan886@gmail.com>
2017-04-14 21:06:05 +0800

    2017年 04月 14日 星期五 21:06:05 CST

--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+#from django.contrib import admin
+from . import view
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$/', view.hello),
 ]

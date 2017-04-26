d9290f253627cd910623ef683b6205bf20b3a067
evan <evan886@gmail.com>
2017-04-26 18:31:39 +0800

    2017年 04月 26日 星期三 18:31:39 CST

--- django/mysite/polls/urls.py
+++ django/mysite/polls/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,20 +1,14 @@
-
-#from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Question
+
 
 def index(request):
-    return  HttpResponse("Hello, word, you are at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)

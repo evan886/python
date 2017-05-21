9be1729752e888e09f2bd2b68a662c114dd63660
root <ruh@yuv.cc>
2017-05-22 02:52:51 +0800

    2017年 05月 22日 星期一 02:52:51 CST

--- django/project/csvt01/blog/views.py
+++ django/project/csvt01/blog/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader, Context
+from django.shortcuts import render_to_response
+'''
 def index(req):
-    return HttpResponse('<h1>hellow welcome </h1>')
-# Create your views here.
+        t = loader.get_template('index.html')
+        c = Context({}) 
+        return HttpResponse(t.render(c))
+        return HttpResponse('<h1>hellow welcome </h1>')
+'''
+
+
+def index(req):
+    return render_to_response('index.html',{})

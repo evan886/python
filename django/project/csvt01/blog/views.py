7e4a6054537e35630c2e3cc824a935a8252b3b80
root <ruh@yuv.cc>
2017-05-22 11:24:54 +0800

    2017年 05月 22日 星期一 11:24:54 CST

--- django/project/csvt01/blog/views.py
+++ django/project/csvt01/blog/views.py
@@ -1,14 +1,18 @@
-from django.http import HttpResponse
-from django.template import loader, Context
+#-*- coding:utf-8 -*-
+#from django.http import HttpResponse
+#from django.template import loader, Context
 from django.shortcuts import render_to_response
-'''
-def index(req):
-        t = loader.get_template('index.html')
-        c = Context({}) 
-        return HttpResponse(t.render(c))
-        return HttpResponse('<h1>hellow welcome </h1>')
-'''
 
-
+class Person(object):
+    def __init__(self,name,age,sex):
+        self.name = name
+        self.age = age
+        self.sex = sex
+    def say(self):
+        return  "I am " + self.name
+#变量定义
 def index(req):
-    return render_to_response('index.html',{})
+    #user = {'name':'tom', 'age':23, 'sex':'male'}
+    user = Person ('max',33,'male')
+    book_list = [ 'python','java','php','web']
+    return render_to_response('index.html',{'title':'my page','user':user,'book_list':book_list})

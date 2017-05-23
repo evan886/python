aaabb991a09183b2a4646a81f9bfb404143662fd
root <ruh@yuv.cc>
2017-05-23 09:24:28 +0800

    2017年 05月 23日 星期二 09:24:28 CST

--- django/project/csvt01/blog/views.py
+++ django/project/csvt01/blog/views.py
@@ -13,6 +13,6 @@ class Person(object):
 #变量定义
 def index(req):
     #user = {'name':'tom', 'age':23, 'sex':'male'}
-    user = Person ('max',33,'male')
+    user = Person ('evan',33,'male')
     book_list = [ 'python','java','php','web']
     return render_to_response('index.html',{'title':'my page','user':user,'book_list':book_list})

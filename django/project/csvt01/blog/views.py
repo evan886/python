d2abd5197939747637e0bb1d30c17ceb6e2099f9
evan <evan886@gmail.com>
2017-05-24 18:38:25 +0800

    2017年 05月 24日 星期三 18:38:25 CST

--- django/project/csvt01/blog/views.py
+++ django/project/csvt01/blog/views.py
@@ -12,7 +12,7 @@ class Person(object):
         return  "I am " + self.name
 #变量定义
 def index(req):
-    #user = {'name':'tom', 'age':23, 'sex':'male'}
-    user = Person ('evan',33,'male')
+    user = {'name':'tom', 'age':23, 'sex':'male'}
+    #user = Person ('evan',33,'male')
     book_list = [ 'python','java','php','web']
     return render_to_response('index.html',{'title':'my page','user':user,'book_list':book_list})

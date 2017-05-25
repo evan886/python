2e5e40814f0443b238652279c1868f795bb9b8b6
evan <evan886@gmail.com>
2017-05-25 19:20:06 +0800

    2017年 05月 25日 星期四 19:20:06 CST

--- django/project/csvt01/blog/views.py
+++ django/project/csvt01/blog/views.py
@@ -11,8 +11,9 @@ class Person(object):
     def say(self):
         return  "I am " + self.name
 #变量定义
-def index(req):
+#def index(req):
+def index(req,id):
     user = {'name':'tom', 'age':23, 'sex':'male'}
     #user = Person ('evan',33,'male')
     book_list = [ 'python','java','php','web']
-    return render_to_response('index.html',{'title':'my page','user':user,'book_list':book_list})
+    return render_to_response('index.html',{'title':'my page','user':user,'book_list':book_list,'id':id})
--- django/project/csvt01/csvt01/urls.py
+++ django/project/csvt01/csvt01/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+#from blog.views import index
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'csvt01.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^blog/index/$','blog.views.index'),
+    #url(r'^blog/index/$','blog.views.index'),
+    #url(r'^blog/index/$',index),
+    #url(r'^blog/index/\d{2}/$','blog.views.index'),
+    url(r'^blog/index/(?P<id>\d{2}/$','blog.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 ]

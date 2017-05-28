0ade69c095c49b28dec9189a8febc83bee34aa10
root <ruh@yuv.cc>
2017-05-28 11:42:39 +0800

    2017年 05月 28日 星期日 11:42:39 CST

--- django/project/csvt02/blog/views.py
+++ django/project/csvt02/blog/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from  django.template import  loader, Context
 from django.http import HttpResponse
 # Create your views here.
+
 def index(req):
     t =loader.get_template('index.html')
+    c = Context({'uname':'evan'})
+    #html=t.render(c)
+    #return HttpResponse(html)
+    return HttpResponse(t.render(c))
+
+def index1(req):
+    
+  
+    
--- django/project/csvt02/csvt02/urls.py
+++ django/project/csvt02/csvt02/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
+
     # url(r'^$', 'csvt02.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^index/$','blog.views.index'),
+    url(r'^index1/$','blog.views.index'),
     url(r'^admin/', include(admin.site.urls)),
 ]

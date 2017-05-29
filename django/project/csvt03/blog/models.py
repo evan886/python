838af39dba20991277dd31f5adb8112b42d5e6bb
root <ruh@yuv.cc>
2017-05-29 09:36:36 +0800

    2017年 05月 29日 星期一 09:36:36 CST

--- django/project/csvt03/blog/models.py
+++ django/project/csvt03/blog/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Employee(models.Model):
-    name = models.CharField
+    name = models.CharField(max_length=20)
--- django/project/csvt03/csvt03/settings.py
+++ django/project/csvt03/csvt03/settings.py
@@ -73,15 +73,19 @@ WSGI_APPLICATION = 'csvt03.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+#https://docs.djangoproject.com/en/1.8/ref/databases/#mysql-notes
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'csvt',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/

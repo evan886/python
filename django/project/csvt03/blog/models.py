cffc152981c1205b724a2d8f412fd373794b63ea
root <ruh@yuv.cc>
2017-05-30 02:31:48 +0800

    2017年 05月 30日 星期二 02:31:48 CST

--- django/project/csvt03/blog/models.py
+++ django/project/csvt03/blog/models.py
@@ -1,5 +1,10 @@
+#-*- coding:utf-8 -*-
 from django.db import models
 
 # Create your models here.
+#自己定义的类
 class Employee(models.Model):
     name = models.CharField(max_length=20)
+    
+    def __unicode__(self):
+        return self.name 

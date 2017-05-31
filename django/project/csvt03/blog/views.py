df075edc004f54aaf734422776e2817e4a3d19cd
evan <evan886@gmail.com>
2017-05-31 19:24:17 +0800

    2017年 05月 31日 星期三 19:24:17 CST

--- django/project/csvt03/blog/views.py
+++ django/project/csvt03/blog/views.py
@@ -1,4 +1,10 @@
 #-*- coding:utf-8 -*-
-from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from  blog.models import Employee
 # Create your views here.
+
+def index(req):
+    emps = Employee.objects.all()
+    return render_to_response('index.html',{'emps':emps})
+
+#render_to_response

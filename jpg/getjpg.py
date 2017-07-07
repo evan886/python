1e6d4a26c4dc60830631fd9338551823c107ab5c
evan <evan886@gmail.com>
2017-07-07 14:17:25 +0800

    2017年 07月 07日 星期五 14:17:25 CST

--- jpg/getjpg.py
+++ jpg/getjpg.py
@@ -3,20 +3,27 @@
 import re
 import urllib
 def getHtml(url):
-    page=urllib.urlopen(url)
+    page = urllib.urlopen(url)
     html = page.read()
-    return  html
+    return html
 
 def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    #reg = r'src="(.*?\.jpg)"  width' #视频中最终用这个，但是我试了不行
     #reg=r"src=" .+\.jpg" width "
-    reg=r'src="(.*?\.jpg)"  width '
-    imgre= re.compile(reg)
-    imglist = re.findall(imgre,html)
-    return imglist
 
-#print getHtml("https://tieba.baidu.com/p/3920117676")
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    x = 0
+    for imgurl in imglist:
+        urllib.urlretrieve(imgurl, '%s.jpg'  % x)
+        x+=1
 
+#html = getHtml("http://tieba.baidu.com/p/2460150866")
+#https://tieba.baidu.com/p/2460150866
 html = getHtml("https://tieba.baidu.com/p/3920117676")
 print getImg(html)
 
-#19
+
+#参考如下 一开始出不来是各种空格问题呀
+# http://www.cnblogs.com/qscqesze/p/4225374.html

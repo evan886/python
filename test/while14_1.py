da9d80ba88e6f59e679be4422e6580c483514fbc
root <ruh@yuv.cc>
2017-07-23 02:20:44 +0800

    2017年 07月 23日 星期日 02:20:44 CST

---
+++ test/while14_1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+x= ""
+while  x != "q":
+    print "hey"
+    x =raw_input("ple input somethin, q for quit:")
+    if not x :
+        break
+    
+else:
+    print "enddo"
+        
+
+        

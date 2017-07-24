a89d0c8388bd5f7fd988a9472ea045d4ca577f70
evan <evan886@gmail.com>
2017-07-24 20:24:06 +0800

    2017年 07月 24日 星期一 20:24:06 CST

--- test/16/1.py
+++ test/16/1.py
@@ -1,7 +1,11 @@
-def fun(x):
-#    if True:
-     print  x
+def fun(x,y):
+    if x == y :
+        print  x,'=' , y
+    else:
+        print  x,'!=' , 
 
 
-s=raw_input("input some:")
-fun(s)        
+
+s1 = raw_input("input some:")
+s2 = raw_input("input some:")
+fun(s1,s2)        
---
+++ test/16/2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#coding:utf8
+def mashine(x=3,y='奶油'):
+    print "生成一个",x,'元',y,'口味的冰'
+
+#mashine(5,'巧克力')    
+mashine(5,)

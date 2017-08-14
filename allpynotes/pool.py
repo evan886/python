ebcbcf829b35a0f0211dbd241baff5d0e1a80ce0
evan <evan886@gmail.com>
2017-08-14 19:57:01 +0800

    2017年 08月 14日 星期一 19:57:01 CST

---
+++ allpynotes/pool.py
@@ -0,0 +1,20 @@
+class Turtle:
+    def __init__(self,x):
+        self.num = x
+
+class  Fish:
+    def __init__(self,x):
+        self.num = x
+
+class Pool:
+    def __init__(self, x , y):
+        self.turtle = Turtle(x)
+        self.fish = Fish(y)
+
+    def print_num(self):
+        #print("the pool have turtle  %d , filsh %d " ) % (self.turtle.num, self.fish.num)
+        printf  %(self.turtle.num)
+
+pool = Pool(1,10)
+pool.print_num()
+        

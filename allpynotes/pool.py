ab9f7d8fd79b34d63cb89aa2616bbfd79bd9ef6d
evan <evan886@gmail.com>
2017-08-15 10:40:44 +0800

    2017年 08月 15日 星期二 10:40:44 CST

--- allpynotes/pool.py
+++ allpynotes/pool.py
@@ -1,20 +1,32 @@
+#!/usr/bin/python
+#-*-coding:utf-8 -*-
 class Turtle:
     def __init__(self,x):
         self.num = x
-
-class  Fish:
+class Fish:
     def __init__(self,x):
         self.num = x
-
 class Pool:
-    def __init__(self, x , y):
+    def __init__(self,x,y):
         self.turtle = Turtle(x)
         self.fish = Fish(y)
 
     def print_num(self):
-        print("the pool have turtle  %d , filsh %d "  % (self.turtle.num, self.fish.num)
-        #print  %(self.turtle.num)
+        print("the pool has turtle   %d, fish %d") %(self.turtle.num, self.fish.num)
 
-pool = Pool(1,10)
+pool =  Pool(1,10)
 pool.print_num()
-        
+
+'''
+evan@evanpc:~/github/python/allpynotes$ python3  pool.py 
+the pool has turtle   %d, fish %d
+Traceback (most recent call last):
+  File "pool.py", line 18, in <module>
+    pool.print_num()
+  File "pool.py", line 15, in print_num
+    print("the pool has turtle   %d, fish %d") %(self.turtle.num, self.fish.num)
+TypeError: unsupported operand type(s) for %: 'NoneType' and 'tuple'
+evan@evanpc:~/github/python/allpynotes$ python  pool.py 
+the pool has turtle   1, fish 10
+
+'''

75833b4b36ade86b03843c7effdc94d74e32b3ff
evan <evan886@gmail.com>
2018-02-22 23:50:49 +0800

    2018年 02月 22日 星期四 23:50:49 CST

--- django/project/laoqi/mysite/mysite/urls.py
+++ django/project/laoqi/mysite/mysite/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include('blog.urls',namespace='blog',app_name='blog')),
-    url(r'^accout/', include('accout.urls',namespace='accout',app_name='accout')),
+    url(r'^account/', include('account.urls',namespace='account',app_name='account')),
 
     #url(r'^$', views.blog_title, name="blog_title"),
 ]

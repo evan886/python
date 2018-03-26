e5a1af51e4654e65bd3d50f87eb31d44ecb50a97
evan <evan886@gmail.com>
2018-03-26 16:18:29 +0800

    2018年 03月 26日 星期一 16:18:28 CST

--- tutorial/1.py
+++ tutorial/1.py
@@ -4,6 +4,7 @@ print  os.getcwd()
 A = os.listdir(os.getcwd())
 print A[+0]
 print os.path.getsize(A[+0])
-def get_FileSize(A[+0])
+#def get_FileSize(A[+0])
+def get_FileSize(filename=keywordargument.py)
   filePath = unicode(filePath,'utf8')
   fsize =  os
---
+++ tutorial/get_filesize.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import os   , sys
+from os.path import join, getsize 
+
+'''
+ [2]: from os.path  import join, getsize 
+In [6]: mysite=getsize ("keywordargument.py")
+In [7]: mb=mysite/float(1024*1024)
+获取文件的大小,结果保留两位小数，单位为MB'''
+
+def get_filesize(filename="note"):
+    #filepath=unicode(filepath,'utf8')
+    #mysize=getsize("filename") # add "" is str 
+    mysize=getsize(filename)/(1024*1024)
+    return round(mysize,2)
+
+#版本1 　可以的
+#print get_filesize("/home/evan/iso/windows10.iso")
+
+#版本2  还有问题
+print get_filesize(str(sys.argv[1:]))

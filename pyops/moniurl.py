c813f2d0b40125964b1658c056bfea7fe251651c
evan <evan886@gmail.com>
2018-10-26 14:16:10 +0800

    2018年 10月 26日 星期五 14:16:10 CST

---
+++ pyops/moniurl.py
@@ -0,0 +1,25 @@
+#-*- coding:utf-8 -*-
+import urllib2
+import time
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_add(s):
+    name, add = parseaddr(s)
+
+from_addr = ""
+to_add = ""
+password = ""
+
+
+def send_email(s):
+    
+    server=
+
+list_of_sites = []
+
+:wq
+
+

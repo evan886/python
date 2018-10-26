a16463f5becef15b41daeea4627a1064aacc5ae0
evan <evan886@gmail.com>
2018-10-26 14:44:38 +0800

    2018年 10月 26日 星期五 14:44:38 CST

---
+++ pyops/urlmoni.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+import urllib2
+import time
+from email.header import Header
+from email.mime.text import MIMEText
+from email.utils import parseaddr, formataddr
+import smtplib
+
+def _format_add(s):
+    name, add = parseaddr(s)
+    return formataddr(( \
+        Header(name,'utf-8').encode(), \                        
+        addr.encode("utf-8") if isinstance(addr,unicode) else addr))     
+
+from_addr = ""
+to_add = ""
+password = ""
+smtp_server = ""
+
+def format_msg(s):
+    #type : (object) ->object
+    msg = MIMEText(s,_subtype='html',_charset='utf-8')
+    msg['From'] = _format_add(from_addr)
+    msg['To'] = _format_add
+
+
+def send_email(s):
+    
+    server=
+
+list_of_sites = []
+
+
+
+

3cc15f31bdb8bbc8a523b72c080f982b9da439e8
evan <evan886@gmail.com>
2018-12-12 13:10:53 +0800

    2018年 12月 12日 星期三 13:10:53 CST

---
+++ learn2018video/cp.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+old_file_name= input(" 输入要复制的文件名 ")
+
+old_file = open(old_file_name,"r")
+
+#test.py  -----> test[复件].py
+#new_file_name = "[复件]"+old_file_name
+position = old_file_name.rfind(".")
+new_file_name = old_file_name[0:position] + "[复件]" + old_file_name[position:]
+
+new_file = open(new_file_name,"w")
+
+while True:
+    content = old_file.read(1024)
+
+    if len(content) ==0:
+        break
+
+    new_file.write(content)
+
+old_file.close()
+new_file.close()
+

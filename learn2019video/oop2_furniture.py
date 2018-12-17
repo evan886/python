6e7bc6d0327f3a51d985f1f5cd4401c1350537b3
evan <evan886@gmail.com>
2018-12-17 23:04:24 +0800

    2018年 12月 17日 星期一 23:04:24 CST

--- learn2019video/oop2_furniture.py
+++ learn2019video/oop2_furniture.py
@@ -4,9 +4,27 @@ class  Home:
         self.area = new_area
         self.info = new_info
         self.addr = new_addr
+        self.left_area = new_area 
 
     def __str__(self):
-        return ""
+        return " 房子的面积是: %d,户型是:%s 地址是 %s"  %(self.area, self.info, self.addr)
+    
+    def add_item(self, item):
+        pass
+    
+class Bed:
+    def __init__(self,new_name,new_area):
+        self.name= new_name
+        self.area = new_area
+
+    def __str__(self):
+         return "%s 占用的面积是 :%d" %(self.name, self.area)
 
 fangzi = Home(129,"三室一厅 ", "北京")
+print(fangzi)
+
+bed1 = Bed(" 席梦思 ",4)
+print(bed1)
+
+fangzi.add_item(bed1)
 print(fangzi)

444db0e8962f41c2c38944ace9d33810d244626b
evan <evan886@gmail.com>
2018-12-18 23:51:06 +0800

    2018年 12月 18日 星期二 23:51:06 CST

--- learn2019video/oop2_furniture.py
+++ learn2019video/oop2_furniture.py
@@ -4,13 +4,17 @@ class  Home:
         self.area = new_area
         self.info = new_info
         self.addr = new_addr
-        self.left_area = new_area 
+        self.left_area = new_area
+        self.contain_items = []
 
     def __str__(self):
-        return " 房子的面积是: %d,户型是:%s 地址是 %s"  %(self.area, self.info, self.addr)
-    
+        msg = " 房子的总面积是: %d,可用面积是 : %d,,户型是:%s 地址是 %s"  %(self.area,self.left_area, self.info, self.addr)
+        msg += "当前房子里面的物品有%s" %(str(self.contain_items))
+        return msg
+    #不太明白这个item.area  c -= a 等效于c = c - a.
     def add_item(self, item):
-        pass
+        self.left_area -=  item.area
+        self.contain_items.append(item.name) # why has name 
     
 class Bed:
     def __init__(self,new_name,new_area):

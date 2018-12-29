0cbb5b40429c916250f972c868ee54eb0fad83f6
evan <evan886@gmail.com>
2018-12-29 22:05:55 +0800

    2018年 12月 29日 星期六 22:05:54 CST

--- learn2019video/oop2_furniture.py
+++ learn2019video/oop2_furniture.py
@@ -11,11 +11,13 @@ class  Home:
         msg = " 房子的总面积是: %d,可用面积是 : %d,,户型是:%s 地址是 %s"  %(self.area,self.left_area, self.info, self.addr)
         msg += "当前房子里面的物品有%s" %(str(self.contain_items))
         return msg
-    #不太明白这个item.area  c -= a 等效于c = c - a.
+    #这个属性会直接得到，不如下面的用方法,  不太明白这个item.area  c -= a 等效于c = c - a.  item 这里实例化是床
     def add_item(self, item):
-        self.left_area -=  item.area
-        self.contain_items.append(item.name) # why has name 
-    
+        #self.left_area -=  item.area
+        #self.contain_items.append(item.name) # why has name
+        
+        self.left_area -= item.get_area()
+        self.contain_items.append(item.get_name)
 class Bed:
     def __init__(self,new_name,new_area):
         self.name= new_name
@@ -23,6 +25,13 @@ class Bed:
 
     def __str__(self):
          return "%s 占用的面积是 :%d" %(self.name, self.area)
+
+    def get_area(self):
+        return self.area
+
+    def get_name(self):
+        return self.name
+     
 
 fangzi = Home(129,"三室一厅 ", "北京")
 print(fangzi)
@@ -32,3 +41,23 @@ print(bed1)
 
 fangzi.add_item(bed1)
 print(fangzi)
+
+
+bed2 = Bed("三人床 ",3)
+fangzi.add_item(bed2)
+print(fangzi)
+
+
+'''
+python3  oop2_furniture.py
+如果用py2 运行是乱码的中文
+
+ 房子的总面积是: 129,可用面积是 : 129,,户型是:三室一厅  地址是 北京当前房子里面的物品有[]
+ 席梦思  占用的面积是 :4
+ 房子的总面积是: 129,可用面积是 : 125,,户型是:三室一厅  地址是 北京当前房子里面的物品有[<bound method Bed.get_name of <__main__.Bed object at 0x7f8988bcc438>>]
+ 房子的总面积是: 129,可用面积是 : 122,,户型是:三室一厅  地址是 北京当前房子里面的物品有[<bound method Bed.get_name of <__main__.Bed object at 0x7f8988bcc438>>, <bound method Bed.get_name of <__main__.Bed object at 0x7f8988bcc518>>]
+
+/home/evan/python/py2018/第1章 python基础/第2节 python语法基础/07.面对对象-1/视频
+18-应用 有点不明白：存放家具.flv
+
+'''

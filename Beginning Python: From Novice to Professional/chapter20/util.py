3e8972dd1d5b912913376b0d2c7c2dae9b6a4b6f
evan <evan886@gmail.com>
2026-04-14 23:21:11 +0800

    Tue Apr 14 11:21:11 PM CST 2026

--- Beginning Python: From Novice to Professional/chapter20/util.py
+++ Beginning Python: From Novice to Professional/chapter20/util.py
@@ -10,3 +10,13 @@ def blocks(file):
         elif block:
             yield ''.join(block).strip()
             block=[]
+
+'''
+
+def lines(file):
+    for line in file:        # 1. 循环读取文件里**每一行**
+        yield line           # 2. 每次返回一行
+                             # 3. 文件读完了 → 循环自动结束
+    yield '\n'               # 4. 循环结束后，**最后再额外返回一个空行**
+
+'''
